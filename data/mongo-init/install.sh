74fd390d0d86b643f28ab6ab30a1a329cc897d0b
David N. Sanchez <dsanchez@dnsai.io>
2021-07-29 09:47:58 -0700

    Reduce the size of the issues data and update the install script to use the new issues.tar.gz file

--- data/mongo-init/install.sh
+++ data/mongo-init/install.sh
@@ -1,3 +1,6 @@
 echo IMPORTING COLLECTION
-mongoimport --authenticationDatabase=admin --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --db=farmdemo --collection=issues --file=/docker-entrypoint-initdb.d/issues.json
+tar -xzvf /docker-entrypoint-initdb.d/issues.tar.gz --directory /tmp
+mongoimport --authenticationDatabase=admin --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --db=farmdemo --collection=issues --file=/tmp/issues.json
+echo "CLEANING UP"
+rm /tmp/issues.json
 echo DONE IMPORTING
